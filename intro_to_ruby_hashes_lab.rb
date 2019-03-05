803c9eeba0b723f4ac1a96753d7dadc9ece919ff
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 23:06:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def monopoly_with_third_tier
 	  :pieces => 4, 
 	  :names => {
 	    :reading_railroad => {},
-	    :pennsylvania_railroad => {}
-	    :b_and_o_railroad => {}
+	    :pennsylvania_railroad => {},
+	    :b_and_o_railroad => {},
 	    :shortline => {} 
 	  },
 	  :rent_in_dollars => {
@@ -46,8 +46,8 @@ monopoly = {:railroads => {
 	  :pieces => 4, 
 	  :names => {
 	    :reading_railroad => {},
-	    :pennsylvania_railroad => {}
-	    :b_and_o_railroad => {}
+	    :pennsylvania_railroad => {},
+	    :b_and_o_railroad => {},
 	    :shortline => {} 
 	  },
 	  :rent_in_dollars => {
